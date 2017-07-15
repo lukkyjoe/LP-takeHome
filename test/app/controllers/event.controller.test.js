fca94410d4f7eae331358b59ab72620f05d225fc
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-15 16:38:47 -0700

    Add save tests.

--- test/app/controllers/event.controller.test.js
+++ test/app/controllers/event.controller.test.js
@@ -35,3 +35,32 @@ describe("Get all events", function(){
         });
     });
 });
+
+// Test will pass if the event is saved
+describe("Post a new event", function(){
+    it("should create new post", function(done){
+        var EventMock = sinon.mock(new Event({ event: 'Save new event from mock'}));
+        var Event = EventMock.object;
+        var expectedResult = { status: true };
+        EventMock.expects('save').yields(null, expectedResult);
+        event.save(function (err, result) {
+            EventMock.verify();
+            EventMock.restore();
+            expect(result.status).to.be.true;
+            done();
+        });
+    });
+    // Test will pass if the event is not saved
+    it("should return error, if post not saved", function(done){
+        var EventMock = sinon.mock(new Event({ event: 'Save new event from mock'}));
+        var event = EventMock.object;
+        var expectedResult = { status: false };
+        EventMock.expects('save').yields(expectedResult, null);
+        event.save(function (err, result) {
+            EventMock.verify();
+            EventMock.restore();
+            expect(err.status).to.not.be.true;
+            done();
+        });
+    });
+});

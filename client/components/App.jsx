0ce4241fe26bba26edc8ad56bba5cf8a391187ef
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-14 17:24:07 -0700

    Add provider and redux setup to App component.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Values } from 'redux-form-website-template';
+import store from './store';
+import showResults from './showResults';
+import SyncValidationForm from './SyncValidationForm';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div style={{textAlign: 'center'}}> 
-        <h1> Working working working </h1>
-      </div>
+      <Provider store={store}>
+        <div style={{ padding: 15 }}>
+          <h2>Synchronous Validation</h2>
+          <SyncValidationForm onSubmit={showResults} />
+          <Values form="syncValidation" />
+        </div>
+      </Provider>
     );
   }
 }

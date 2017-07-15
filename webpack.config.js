4bdca303aacb96ecb341152bb38db985419e35a7
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-14 20:23:21 -0700

    Change loader key to use key with diff value.

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
         test: /\.css$/,
-        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+        use: [ 'style-loader', 'css-loader' ]
       }
     ]
   },

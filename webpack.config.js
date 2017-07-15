0bfba72d2fb0e9499ffdbf22030874c792b99668
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-14 20:20:43 -0700

    Add css loader to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,11 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.css$/,
+        loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
+      }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]

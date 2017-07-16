07996717e7f3f080be7cad8e49f9763dc219299f
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-15 21:16:51 -0700

    Create save file.

---
+++ client/saveToDB/save.js
@@ -0,0 +1,14 @@
+const Event = require('../model/event.model.js');
+
+module.exports.allEvents = function(req, res) {
+  Event.find({}, function(err, events) {
+    res.send(events);
+  })
+};
+
+module.exports.saveToDB = function(obj) {
+  Event.create({firstName: obj.firstName, lastName: obj.lastName, email: obj.email, date: obj.date}, function(err, event) {
+    if (err) {console.error(err)}
+    else {console.log(event)}
+  })
+}

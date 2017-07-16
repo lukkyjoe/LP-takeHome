a51c8358aba741963455ced935079a7ed70520ed
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-16 08:28:37 -0700

    Add consts for testing.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,18 +3,30 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Values } from 'redux-form-website-template';
 import store from './store';
-import showResults from './showResults';
+// import showResults from './showResults';
 import submitResults from './submitResults';
 import SyncValidationForm from './SyncValidationForm';
 import DatePicker from './DatePicker';
 
+const test = () => console.log('hello it is me'); 
+
+const showResults = values =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      // simulate server latency
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+      resolve()
+    }, 500)
+  })
+
 export default class App extends React.Component {
+
   render() {
     return (
       <Provider store={store}>
         <div style={{ padding: 15 }}>
           <h2>Application for event</h2>
-          <SyncValidationForm onSubmit={submitResults} />
+          <SyncValidationForm onSubmit={test} />
 
         </div>
       </Provider>

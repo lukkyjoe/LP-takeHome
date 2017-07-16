dd46793e8b9e11a22da420726cf79c8554557abe
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-16 11:24:10 -0700

    Update validation code.

--- client/components/SyncValidationForm.js
+++ client/components/SyncValidationForm.js
@@ -12,22 +12,16 @@ momentLocaliser(moment)
 
 const validate = values => {
   const errors = {}
-  if (!values.username) {
-    errors.username = 'Required'
-  } else if (values.username.length > 15) {
-    errors.username = 'Must be 15 characters or less'
+  if (!values.firstName) {
+    errors.firstName = 'Required'
+  }
+  if (!values.lastName) {
+    errors.firstName = 'Required'
   }
   if (!values.email) {
     errors.email = 'Required'
   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
     errors.email = 'Invalid email address'
-  }
-  if (!values.age) {
-    errors.age = 'Required'
-  } else if (isNaN(Number(values.age))) {
-    errors.age = 'Must be a number'
-  } else if (Number(values.age) < 18) {
-    errors.age = 'Sorry, you must be at least 18 years old'
   }
   return errors
 }
@@ -84,13 +78,19 @@ const SyncValidationForm = props => {
   return (
     <form onSubmit={handleSubmit}>
       <Field
-        name="username"
+        name="firstName"
         type="text"
         component={renderField}
-        label="Username"
+        label="First Name"
+      />
+    <form onSubmit={handleSubmit}>
+      <Field
+        name="lastName"
+        type="text"
+        component={renderField}
+        label="First Name"
       />
       <Field name="email" type="email" component={renderField} label="Email" />
-      <Field name="age" type="number" component={renderField} label="Age" />
       <Field name="date" showTime={false} component={renderDateTimePicker} />
       <div>
         <button type="submit" disabled={pristine || submitting}>

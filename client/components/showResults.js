54dae8dd86f2d094efd5e59bdf5f9e39eb757f28
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-16 09:09:10 -0700

    Change showResults to promise format.

--- client/components/showResults.js
+++ client/components/showResults.js
@@ -1,6 +1,10 @@
-const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+const showResults = values =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      // simulate server latency
+      window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`)
+      resolve()
+    }, 500)
+  })
 
-export default (async function showResults(values) {
-  await sleep(500); // simulate server latency
-  window.alert(`You submitted:\n\n${JSON.stringify(values, null, 2)}`);
-});
+export default showResults;

b18a1e196709923114786b44633f27ba80b0d236
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-15 21:16:57 -0700

    Create save test.

---
+++ test/saveTest.js
@@ -0,0 +1,31 @@
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+var save = require('../client/saveToDB/save');
+var Event = require('../client/model/event.model');
+
+describe('save', function() {
+	beforeEach(function() {
+		sinon.stub(Event, 'find');
+  });
+  
+	afterEach(function() {
+		Event.find.restore();
+	});
+
+	it('should send all events', function() {
+		var a = { name: 'a' };
+		var b = { name: 'b' };
+		var expectedModels = [a, b];
+		Event.find.yields(null, expectedModels);
+		var req = { params: { } };
+		var res = {
+			send: sinon.stub()
+		};
+
+		save.allEvents(req, res);
+
+		sinon.assert.calledWith(res.send, expectedModels);
+	});
+
+});

fd0f9645ebcd458036723740b6f694b40fb1d868
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-15 20:16:55 -0700

    Write mongo save method.

--- client/components/submitResults.js
+++ client/components/submitResults.js
@@ -1,8 +1,8 @@
-//import mongoose save methods
+const Save = require('../saveToDB/save');
 
 export default function submitResults(values) {
   console.dir(values);
 
-  // save into mongoDB
+  Save.saveToDB(values);
 
 }

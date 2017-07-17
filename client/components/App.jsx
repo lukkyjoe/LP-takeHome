b195b50edab4bf549e644df470c77f4ea2289cbd
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-16 18:50:47 -0700

    Comment out material-ui imports.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,17 +8,21 @@ import submitResults from './submitResults';
 import SyncValidationForm from './SyncValidationForm';
 import DatePicker from './DatePicker';
 
+// import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// import getMuiTheme from 'material-ui/styles/getMuiTheme';
+// import AppBar from 'material-ui/AppBar';
+
 export default class App extends React.Component {
 
   render() {
     return (
-      <Provider store={store}>
-        <div style={{ padding: 15 }}>
-          <h2>Application for an Event</h2>
-          <SyncValidationForm onSubmit={submitResults} />
-
-        </div>
-      </Provider>
+        <Provider store={store}>
+          <div style={{ padding: 15 }}>
+            <h2>Application for an Event</h2>
+            <SyncValidationForm onSubmit={submitResults} />
+          </div>
+        </Provider>
     );
   }
 }

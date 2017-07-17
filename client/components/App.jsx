4005f29a69871e71c8e983dbe52b02e81d4f2169
lukkyjoe <lukkyjoe@MacBooks-MacBook-459.local>
2017-07-16 22:18:34 -0700

    Add getMuiTheme and provider.

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -8,6 +8,10 @@ import submitResults from './submitResults';
 import SyncValidationForm from './SyncValidationForm';
 import DatePicker from './DatePicker';
 
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+
 // import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 // import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 // import getMuiTheme from 'material-ui/styles/getMuiTheme';
@@ -18,10 +22,12 @@ export default class App extends React.Component {
   render() {
     return (
         <Provider store={store}>
-          <div style={{ padding: 15 }}>
-            <h2>Application for an Event</h2>
-            <SyncValidationForm onSubmit={submitResults} />
-          </div>
+          <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <div style={{ padding: 15 }}>
+              <h2>Application for an Event</h2>
+              <SyncValidationForm onSubmit={submitResults} />
+            </div>
+          </MuiThemeProvider>
         </Provider>
     );
   }
